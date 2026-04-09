70c982aea8ca1111547b495e95f92ae4d49c2914
RayanEmad634 <rayanemad634@gmail.com>
2026-04-09 03:25:19 +0200

    Phase 1 Completed ,Exceptions Still unhandled

--- src/GradeAnalayzer.java
+++ src/GradeAnalayzer.java
@@ -3,7 +3,35 @@ import java.util.ArrayList;
 public class GradeAnalayzer extends GradeProcessor {
     @Override
     public String processGrades(ArrayList<Double> grades) {
+        validateGrades(grades);
         // TO DO: TMRW HIGHEST GRADE, LOWEST GRADE, AVERAGE GRADE, STANDARD DEVIATION
-        return "";
+        String s = "";
+        double min = grades.getFirst();
+        double max = grades.getFirst();
+        double sum = 0;
+        double count = 0;
+        // max,min,avg
+        for (Double grade : grades)
+        {
+            if (max < grade)
+            {
+                max = grade;
+            }
+            if (min > grade)
+            {
+                min = grade;
+            }
+            sum += grade;
+            count++;
+        }
+        double avg = sum / count;
+        double standardDeviationSum = 0;
+        for (Double grade : grades)
+        {
+            standardDeviationSum += Math.pow(grade - avg, 2); // (x-x(avg))^2
+        }
+        double standardDeviation = Math.sqrt(standardDeviationSum/count);
+        return String.format("Highest Grade: %f, Lowest Grade: %f, Average Grade: %f, Standard Deviation: %f"
+                , max,min,avg,standardDeviation);
     }
 }
--- src/GradeProcessor.java
+++ src/GradeProcessor.java
@@ -3,15 +3,18 @@ import java.util.ArrayList;
 public abstract class GradeProcessor {
     public abstract String processGrades(ArrayList <Double> grades);
 
-    public boolean validateGrades(ArrayList<Double> grades)
+    public void validateGrades(ArrayList<Double> grades)
     {
+        if (grades.isEmpty())
+        {
+            throw new InvalidGradeException("Empty Grades");
+        }
         for (Double grade : grades)
         {
             if (grade > 100 || grade < 0)
             {
-                throw new InvalidGradeException("Invalid Grade");
+                throw new InvalidGradeException("List Contains an Invalid Grade");
             }
         }
-        return true;
     }
 }
