d23c7b1d56fbca902e507ec84a90b4cbfbf78e01
RayanEmad634 <rayanemad634@gmail.com>
2026-04-09 03:51:55 +0200

    Gave up on adding threads, so just gonna try do file i/o

---
+++ src/thread/StudentFileReader.java
@@ -0,0 +1,9 @@
+package thread;
+
+//• Reads student data from a file
+//• Each thread processes ONE file
+//• Files contain: studentId, name, and comma separated grades
+
+public class StudentFileReader extends Thread {
+
+}
